5ca9828bc51bc8995d21ce41dc108969800a1ead
muhamadreza1706 <mrezap1706@gmail.com>
2024-11-01 13:38:01 +0700

    Membuat navbar responsive

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,16 +11,55 @@
     <link rel="stylesheet" href="../css/app.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    {{-- BOOTSTRAP CDN  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     
 </head>
 <body>
-    <p>INI HALAMAN BLOG</p>
-    <a href="/">Home</a>
-    <a href="/services">Services</a>
-    <a href="/portfolio">Portfolio</a>
-    <a href="/marketplace">Marketplace</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
+    {{-- NAVBAR  --}}
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('images/althea-logo.png') }}" alt="logo" class="logo"/>
+                Althea
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="/services" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Services
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Website Development</a></li>
+                            <li><a class="dropdown-item" href="#">Software Development</a></li>
+                            <li><a class="dropdown-item" href="#">Game Developer</a></li>
+                            <li><a class="dropdown-item" href="#">Hosting</a></li>
+                            <li><a class="dropdown-item" href="#">SEO</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/marketplace">Marketplace</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/blog">Blog</a>
+                    </li>
+                </ul>
+                <button class="get-started" type="submit">Get Started</button>
+            </div>
+        </div>
+    </nav>
 
     {{-- JAVASCRIPT --}}
     <script src="../js/app.js"></script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,49 +11,52 @@
     <link rel="stylesheet" href="../css/app.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    {{-- BOOTSTRAP CDN  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     
 </head>
 <body>
-    {{-- <p>INI HALAMAN HOME</p>
-    <a href="/">Home</a>
-    <a href="/services">Services</a>
-    <a href="/portfolio">Portfolio</a>
-    <a href="/marketplace">Marketplace</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a> --}}
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    {{-- NAVBAR  --}}
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar scroll</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('images/althea-logo.png') }}" alt="logo" class="logo"/>
+                Althea
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarScroll">
-                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Link
+                        <a class="nav-link dropdown-toggle" href="/services" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Services
                         </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Website Development</a></li>
+                            <li><a class="dropdown-item" href="#">Software Development</a></li>
+                            <li><a class="dropdown-item" href="#">Game Developer</a></li>
+                            <li><a class="dropdown-item" href="#">Hosting</a></li>
+                            <li><a class="dropdown-item" href="#">SEO</a></li>
+                        </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Link</a>
+                        <a class="nav-link" aria-current="page" href="/portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/marketplace">Marketplace</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/blog">Blog</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <button class="get-started" type="submit">Get Started</button>
             </div>
         </div>
     </nav>
